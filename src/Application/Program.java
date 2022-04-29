3a0fadd784cc897f96f4b555ac035a26376af897
SauloCunha01 <saulocunha01@hotmail.com>
2022-04-29 20:34:48 -0300

    Classe Seller

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -5,7 +5,9 @@
  */
 package Application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 /**
  *
@@ -15,5 +17,7 @@ public class Program {
     public static void main(String[]args){
         Department obj = new Department(1,"Books");
         System.out.println(obj);
+        Seller seller = new Seller(21,"bob","bob@gmail.com", new Date(), 3000.0,obj );
+        System.out.println(seller);
     }
 }
